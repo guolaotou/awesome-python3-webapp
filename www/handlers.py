1f60255c237cccd6e36fc7499d6286b0dd6775ae
郭宇 <danse@guoyudeMacBook-Pro.local>
2018-01-07 21:22:49 +0800

    构建前端(Day8)采用Uikit框架，成功跑起来了较为美观的博客主页

--- www/handlers.py
+++ www/handlers.py
@@ -11,10 +11,25 @@ from coroweb import get
 from models import User, Comment, Blog, next_id
 
 
+#day 7
+# @get('/')
+# async def index(request):
+#     users = await User.findAll()
+#     return {
+#         '__template__': 'test.html',
+#         'users': users
+#     }
+
+#day 8(构建前端)
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
